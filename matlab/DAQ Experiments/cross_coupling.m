5cdd78de01cdc8883cb288fb470d939e5dda77dc
Kuba Kowalewski <43864899+KubaSrc@users.noreply.github.com>
2024-01-16 14:01:52 -0500

    New characterization figures

--- matlab/DAQ Experiments/cross_coupling.m
+++ matlab/DAQ Experiments/cross_coupling.m
@@ -43,6 +43,50 @@ plot(t,input_pos);
 if save_data
     save('theta_12.mat','input_pos','output_pos', 't');
 end
+
+%% PLotting function
+data = load('theta_12.mat');
+export_fig = true;
+fig_s = 3;
+% fonts
+ax_font_size = 9*fig_s;
+legend_font_size = 9*fig_s;
+
+fig = figure(1); clf; hold on;
+grid on
+map = brewermap(9,'Set1');
+
+motor_on = find(abs(data.input_pos)>0);
+data.t = data.t(motor_on:end,:);
+data.t = data.t - data.t(1);
+data.input_pos = data.input_pos(motor_on:end,:);
+data.output_pos = data.output_pos(motor_on:end,:);
+
+plot(data.t,data.input_pos./(2*pi),'LineWidth',3,'color',map(1,:),"DisplayName","")
+plot(data.t,data.output_pos./(2*pi),'LineWidth',3,'color',map(2,:),"DisplayName","")
+
+% figure formatting
+set(gcf,'color','w');
+set(fig, 'Units', 'inches');
+width = 2.25;
+height = 1.75;
+set(fig, 'Position', [0, 0, width*fig_s, height*fig_s]);
+
+% axis formatting
+set(findobj(gcf,'type','axes'),'FontSize',ax_font_size,'LineWidth',1.5);
+xlim([0 15])
+ylim([-5 25])
+
+lg = legend('interpreter','latex','Location','northwest');
+lg.FontSize = legend_font_size;
+set(lg,'Box','off')
+lg.ItemTokenSize(1) = 20;
+
+% export fig
+if export_fig
+    exportgraphics(gcf,'./cross-coupling.png','Resolution',300*fig_s)
+end
+
 %% Function for decoding signal from encoders
 function pos_array = decodeQuadrature(signalA, signalB)
     pos_array = zeros(size(signalA));
