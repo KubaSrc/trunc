018fffe6e76bb0596701e81bfc3c1c3b39b65aab
Kuba Kowalewski <43864899+KubaSrc@users.noreply.github.com>
2024-02-05 12:59:50 -0500

    New model class structure

---
+++ bot_LSTM.py
@@ -0,0 +1,195 @@
+from bot import *
+
+class aux_bot(aux_bot):
+
+    def __init__(self,drive_path = None, pos_path = None, motor_path = None,
+                 train_forward = False, train_inverse = False, normalize=True):
+
+        # Size attributes
+        MAX_SEQ_LENGTH = 80
+        BATCH_SIZE = 16
+
+        super().__init__(drive_path, pos_path, motor_path,
+                 train_forward, train_inverse,
+                 normalize,BATCH_SIZE,MAX_SEQ_LENGTH,model_type='DNN',)
+
+        # Constant for forward model
+        self.EPOCHS = 100
+        self.LEARNING_RATE = 0.0005
+        self.MOMENTUM = 0.9
+        self.WEIGHT_DECAY = 0
+        self.DROPOUT = 0
+        self.LAYERS = 1
+
+        # Train forward model
+        if  train_forward:
+            self.fk_net = self.forward_net(inputs = self.input_size,
+                                           hidden_lstm = 512,
+                                           hidden = 1552,
+                                           outputs = self.output_size,
+                                           num_layers = 1,
+                                           dropout = self.DROPOUT,
+                                           device = self.device)
+
+            self.fk_net.load_state_dict(torch.load(drive_path + '/models/forward_2024_02_02-16_40_20_3.213mm'))
+
+            # Train and evaluate
+            self.train_forward(self.fk_net,self.EPOCHS,self.LEARNING_RATE,self.MOMENTUM,self.WEIGHT_DECAY,annealing=True)
+            self.eval_model_forward(self.fk_net,self.fk_train_loss)
+
+        # Load forward model instead
+        if not train_forward:
+            self.fk_net = self.forward_net(hidden_lstm=512, hidden=1552)
+            self.fk_net.load_state_dict(torch.load(drive_path + '/models/forward_2024_02_02-16_40_20_3.213mm'))
+            self.fk_net.eval()
+            print("[aux_net] Forward model succesfully loaded")
+
+        # Constant for inverse model
+        self.EPOCHS = 50
+        self.LEARNING_RATE = 0.001
+        self.MOMENTUM = 0.9
+        self.DROPOUT = 0
+        self.LAYERS = 1
+        self.GAMMA = 0.9
+
+        # Train inverse model
+        if  train_inverse:
+            self.ik_net = self.inverse_net(hidden=1552,
+                                           hidden_lstm = 512,
+                                           inputs=self.output_size,
+                                           outputs=self.input_size,
+                                           device=self.device,
+                                           dropout=self.DROPOUT,
+                                           num_layers=self.LAYERS,
+                                           linear_depth=3)
+            # Bootstrap off previous run
+            self.ik_net.load_state_dict(torch.load(drive_path + '/models/inverse_2024_02_02-17_46_11_14.739pwm'))
+
+            # Train and evaluate
+            self.train_inverse(self.ik_net,self.EPOCHS,self.LEARNING_RATE,self.MOMENTUM,self.WEIGHT_DECAY,annealing=True)
+            self.eval_model_inverse(self.ik_net,self.ik_train_loss)
+
+        # Load inverse model instead
+        if not train_inverse:
+            self.ik_net = self.inverse_net(hidden_lstm=512, hidden=1552,device=self.device,linear_depth=3)
+            self.ik_net.load_state_dict(torch.load(drive_path + '/models/inverse_2024_02_04-22_48_17_10.251mm'))
+            self.ik_net.eval()
+            self.ik_net.to(self.device)
+            print("[aux_net] Inverse model succesfully loaded")
+
+    ########################
+    # FORWARD NETWORK
+    ########################
+
+    class forward_net(nn.Module):
+        def __init__(self, inputs=9, hidden_lstm=512, hidden=1024, outputs=7,num_layers=1,dropout=0,device=None):
+            super().__init__()
+            self.device = device
+
+            self.lstm = nn.LSTM(
+            input_size=inputs,
+            hidden_size=hidden_lstm,
+            batch_first=True,
+            num_layers=num_layers,
+            )
+
+            self.fc1 = nn.Linear(2 * hidden_lstm, hidden)
+            self.fc2 = nn.Linear(hidden, hidden)
+            self.fc3 = nn.Linear(hidden, outputs)
+
+            self.input_size = inputs
+            self.hidden_size = hidden
+            self.hidden_lstm_size = hidden_lstm
+            self.output_size = outputs
+            self.num_layers = num_layers
+            self.dropout = nn.Dropout(dropout)
+
+        def forward(self,x):
+            batch_size = x.shape[0]
+            h0 = torch.zeros(self.num_layers, batch_size, self.hidden_lstm_size).requires_grad_().to(self.device)
+            c0 = torch.zeros(self.num_layers, batch_size, self.hidden_lstm_size).requires_grad_().to(self.device)
+
+            _, (hn, cn) = self.lstm(x, (h0, c0))
+
+            y = torch.cat((hn[-1], cn[-1]), dim=1)
+
+            y = F.relu(y)
+            y = self.fc1(y)
+            y = F.relu(y)
+            y = self.fc2(y)
+            y = F.relu(y)
+            y = self.fc3(y)
+
+            return y
+
+    #########################
+    ## INVERSE NETWORK
+    #########################
+
+    class inverse_net(nn.Module):
+        def __init__(self, inputs=7, hidden_lstm=512, hidden=1024, outputs=9, num_layers=1, dropout=0.1, device=None, linear_depth = None):
+            super().__init__()
+
+            self.device = device
+
+            self.lstm = nn.LSTM(
+            input_size=inputs,
+            hidden_size=hidden_lstm,
+            batch_first=True,
+            num_layers=num_layers,
+            )
+
+            self.drop = nn.Dropout(dropout)
+
+            # Default size
+            if linear_depth == None:
+                self.fc1 = nn.Linear(hidden_lstm,hidden)
+                self.fc2 = nn.Linear(hidden,hidden)
+                self.fc3 = nn.Linear(hidden,outputs)
+
+            # Explore different sizes
+            else:
+                self.linear_layers = nn.ModuleList()  # Use nn.ModuleList
+                if linear_depth == 1:
+                    self.linear_layers.append(nn.Linear(2 * hidden_lstm, outputs))
+                else:
+                    self.linear_layers.append(nn.Linear(2 * hidden_lstm, hidden))
+                    for _ in range(1, linear_depth - 1):
+                        self.linear_layers.append(nn.Linear(hidden, hidden))
+                    self.linear_layers.append(nn.Linear(hidden, outputs))
+
+            self.input_size = inputs
+            self.hidden_size = hidden
+            self.hidden_lstm_size = hidden_lstm
+            self.output_size = outputs
+            self.num_layers = num_layers
+            self.dropout = dropout
+            self.linear_depth = linear_depth
+
+        def forward(self,y):
+
+            batch_size = y.shape[0]
+            h0 = torch.zeros(self.num_layers, batch_size, self.hidden_lstm_size).requires_grad_().to(self.device)
+            c0 = torch.zeros(self.num_layers, batch_size, self.hidden_lstm_size).requires_grad_().to(self.device)
+
+            _, (hn, cn) = self.lstm(y, (h0, c0))
+            x = torch.cat((hn[-1], cn[-1]), dim=1)
+
+            x = self.drop(x)
+
+            # Default architecture
+            if self.linear_depth == None:
+                x = F.relu(x)
+                x = self.fc1(x)
+                x = F.relu(x)
+                x = self.fc2(x)
+                x = F.relu(x)
+                x = self.fc3(x)
+
+            # n-dimensional
+            else:
+                for fc in self.linear_layers:
+                    x = F.relu(x)
+                    x = fc(x)
+
+            return x
---
+++ matlab/training/workspace_analysis.m
@@ -0,0 +1,138 @@
+close all; clear; clc;
+
+% Read the data from CSV file
+
+T = readtable("./data/2024_02_03_14_22_02/positions_trim.csv");
+
+% Extract X, Y, and Z data points
+X = (T.x_end_avg).*1000; 
+Y = (T.y_end_avg).*1000;
+Z = (T.z_end_avg).*1000;
+
+quat = [T.qw_end_avg,T.qx_end_avg,T.qy_end_avg,T.qz_end_avg];
+quat = quaternion(quat);
+pose_dist = xy_dist(quat);
+pose_dist = rad2deg(pose_dist);
+quat_new = convert_quat(quat);
+
+map = brewermap(9,'Set1');
+
+%% Histogram of tilt
+
+figure(1); clf; hold on;
+
+hist(pose_dist)
+ylabel('Count')
+xlabel('Tilt (degree)')
+
+%% Visualize the pose
+
+n = 100;
+
+plot_triad(X(1:n),Y(1:n),Z(1:n),quat_new(1:n))
+
+%% Poses that are close to normal to the plane
+
+
+pose_slice = pose_dist < 5;
+
+plot_triad(X(pose_slice),Y(pose_slice),Z(pose_slice),quat(pose_slice))
+
+%% Helper functions
+
+function angles = xy_dist(quaternions)
+    % Initialize the angles array
+    angles = zeros(size(quaternions, 1), 1);
+    
+    % Reference up-vector (local Z-axis)
+    upVector = [0; 0; 1];
+    
+    for idx = 1:size(quaternions, 1)
+        % Extract the quaternion
+        q = quaternions(idx, :);
+        
+        % Convert quaternion to rotation matrix
+        R = quat2rotm(q);
+        
+        % Rotate the up-vector
+        rotatedVector = R * upVector;
+        
+        % Calculate the angle with the Z-axis (dot product)
+        cosTheta = dot(rotatedVector, [0; 0; 1]) / norm(rotatedVector);
+        % Ensure the value is within the valid range for acos
+        cosTheta = max(min(cosTheta, 1), -1);
+        angle = acos(cosTheta);
+        
+        % Store the angle
+        angles(idx) = angle;
+    end
+end
+
+
+function R = rotx(theta)
+    % Define the rotation matrix
+    R = [1, 0, 0;
+         0, cos(theta), -sin(theta);
+         0, sin(theta), cos(theta)];
+end
+
+function R = roty(theta)
+    % Define the rotation matrix
+    R = [cos(theta), 0, sin(theta);
+         0, 1, 0;
+         -sin(theta), 0, cos(theta)];
+end
+
+function R = rotz(theta)
+    % Define the rotation matrix
+    R = [cos(theta), -sin(theta), 0;
+         sin(theta), cos(theta), 0;
+         0, 0, 1];
+end
+
+
+function q_new = convert_quat(q)
+
+q_new = zeros(length(q),4);
+
+for idx = 1:length(q)
+    e = quat2eul(q(idx),'XYZ');
+    rx = rotx(e(1));
+    ry = roty(e(2));
+    rz = rotz(e(3));
+    r_new = rx*ry*rz;
+    q_new(idx,:) = rotm2quat(r_new);
+end
+
+q_new = quaternion(q_new);
+
+end
+
+function plot_triad(x, y, z, q)
+    figure(); clf; hold on; grid on; axis equal;
+    xlabel('X'); ylabel('Y'); zlabel('Z');
+    
+    arrowLength = 20; % Adjust the arrow length as needed
+    headSize = 20;
+        
+
+    for i = 1:length(x)
+        % Convert quaternion to rotation matrix
+        R = quat2rotm(q(i,:));
+        
+        % Origin for the triad
+        origin = [x(i), y(i), z(i)];
+        
+        % Directions for the triad arrows, transformed by R
+        xDir = R(:,1)';
+        yDir = R(:,2)';
+        zDir = R(:,3)';
+        
+        % Plot arrows
+        quiver3(origin(1), origin(2), origin(3), xDir(1), xDir(2), xDir(3), arrowLength, 'r', 'LineWidth', 2, 'MaxHeadSize', headSize);
+        quiver3(origin(1), origin(2), origin(3), yDir(1), yDir(2), yDir(3), arrowLength, 'g', 'LineWidth', 2, 'MaxHeadSize', headSize);
+        quiver3(origin(1), origin(2), origin(3), zDir(1), zDir(2), zDir(3), arrowLength, 'b', 'LineWidth', 2, 'MaxHeadSize', headSize);
+    end
+    
+    hold off;
+end
