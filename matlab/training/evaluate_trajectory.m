0c8b2a6e0599528de10648eba1375cca7bbf9244
Kuba Kowalewski <43864899+KubaSrc@users.noreply.github.com>
2025-01-23 16:01:49 -0500

    Recorded data for evaluating the trajectory

--- matlab/training/evaluate_trajectory.m
+++ matlab/training/evaluate_trajectory.m
@@ -10,7 +10,7 @@ home_pos = load('./state/home_measured.mat').pos;
 
 T_ideal = load('./inference/655g_circle_trajectory_IDEAL.mat');
 T_ideal = T_ideal.wp;
-T_model = readtable('./experiments/circle_2025_01_14_15_50_21/positions.csv');
+T_model = readtable('./experiments/circle_originalWMass2025_01_23_14_52_29/positions.csv');
 
 
 % Ideal points
--- matlab/training/follow_trajectory.m
+++ matlab/training/follow_trajectory.m
@@ -15,7 +15,7 @@ cam_idx = 2;
 
 % Switch to programmed path
 programed = false;
-programmed_path = './inference/655g_all_data_with_mass_inputs.mat';
+programmed_path = './inference/655g_all_data_zeroed_inputs.mat';
 
 if ~ programed
     trajectory_name = ['./inference/',trajectory,'_trajectory.mat'];
@@ -25,7 +25,7 @@ if ~ programed
     save_points_name = ['./inference/',trajectory,'_p_'];
 else
     trajectory_name = "./inference/655g_weighted_circle_trajectory.mat";
-    inputs_name = "./inference/655g_all_data_with_mass_inputs.mat";
+    inputs_name = "./inference/655g_all_data_zeroed_inputs.mat";
 end
 
 %% Initial setup
